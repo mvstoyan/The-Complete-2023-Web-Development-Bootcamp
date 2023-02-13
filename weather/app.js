007e5f4dd967bff001fb15d513fa782bf6570834
Marina Tilton <m.v.stoyan@gmail.com>
2023-02-13 16:21:09 -0700

    weather

---
+++ weather/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "042145cb758879d9a3765fc147b487d7";
+    const unit = "imperial";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription= weatherData.weather[0].description; //weather is an array with one entry and what's why we use weather[0]
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " F.</h1>");
+            res.write("<img src=" + imageURL +">");
+            res.send();
+        });
+    });
+
+});
+
+    
+app.listen(3030, function() {
+    console.log("Server is running on port 3030.");
+});
